5f183669ecc71282599957a2258ef6b00c834128
juan.ahmed <juan.ahmed@divcom.ch>
2024-05-08 11:17:48 +0200

    Etape 1

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,18 @@
 <script setup>
 // Cheat Sheet: https://steve-fallet.notion.site/Vue-3-script-setup-Cheat-Sheet-b12192ceae244ecda65f771579ca02bc
-import {ref} from 'vue'
+import {onMounted, ref} from 'vue'
 
 // Tableau des troupes
-const troupes = ref([
-  {
-    "id": 2,
-    "nom": "Archer",
-    "niveau": 3,
-    "formation": 25,
-    "vitesse": 24,
-    "cout": 200,
-    "description": "Les archers sont des tireurs d'élite qui attaquent à distance. Ils sont rapides et bon marché à former, mais ils sont faibles en mêlée et doivent être protégés.",
-    "couleur": "#EE5487",
-    "image": "https://cocapi.divtec.me/img/archer.png"
-  },
-  {
-    "id": 1,
-    "nom": "Barbare",
-    "niveau": 4,
-    "formation": 20,
-    "vitesse": 16,
-    "cout": 150,
-    "description": "Ce guerrier intrépide compte sur ses muscles saillants et sa fière moustache pour semer le chaos dans les villages ennemis. Faites charger une horde de barbares et profitez du spectacle !",
-    "couleur": "#ebb428",
-    "image": "https://cocapi.divtec.me/img/barbare.png"
-  },
-  {
-    "id": 3,
-    "nom": "Géant",
-    "niveau": 5,
-    "formation": 12,
-    "vitesse": 12,
-    "cout": 2250,
-    "description": "Les géants sont de grands guerriers qui attaquent les défenses ennemies en premier. Ils sont lents mais résistants.",
-    "couleur": "#F6901A",
-    "image": "https://cocapi.divtec.me/img/geant.png"
-  }
-])
+const troupes = ref([])
+
+// Quand le composant est monté, on va chercher les données
+onMounted(() => {
+  fetch('https://cocapi.divtec.me/troupes') //Appel à l'API
+      .then((reponseAPI) => reponseAPI.json())  //Récupération des données au format .json et transformation en objet JavaScript
+      .then((donneesFormatJS) => { //Récupération des données au format JavaScript
+        troupes.value = donneesFormatJS //Stockagge dans la variable troupes
+      })
+})
 </script>
 
 <template>
@@ -72,7 +47,7 @@ const troupes = ref([
             Niveau {{troupe.niveau}}
           </div>
           <h2 class="name">{{ troupe.nom }}</h2>
-          <button style="background-color: #EE5487;"> Former
+          <button :style="'background-color: ' + troupe.couleur + ';'"> Former
             <img src="/img/piece-or.png" alt="Former"></button>
           <p class="description">
             {{troupe.description}}</p>
