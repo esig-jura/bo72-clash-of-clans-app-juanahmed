60ae20a381f27e15f09fc9f9848588bf84b1def1
juan.ahmed <juan.ahmed@divcom.ch>
2024-05-20 15:52:20 +0200

    Etape 1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup>
 // Cheat Sheet: https://steve-fallet.notion.site/Vue-3-script-setup-Cheat-Sheet-b12192ceae244ecda65f771579ca02bc
-import {ref} from 'vue'
+//import {ref} from 'vue'
+import {onMounted, ref} from 'vue'
+
+// Quand le composant est monté, on va chercher les données
+onMounted(() => {
+  fetch('https://cocapi.divtec.me/troupes') //Appel à l'API
+      .then((reponseAPI) => reponseAPI.json())  //Récupération des données au format .json et transformation en objet JavaScript
+      .then((donneesFormatJS) => { //Récupération des données au format JavaScript
+        troupes.value = donneesFormatJS //Stockagge dans la variable troupes
+      })
+})
 
 // Tableau des troupes
 const troupes = ref([
@@ -64,34 +74,31 @@ const troupes = ref([
     <ul class="cartes" v-for="troupe in troupes">
       <li>
         <article>
-          <header style="background: linear-gradient(60deg,#3B3B3B 0%, #EE5487 100%);">
-            <img src="https://cocapi.divtec.me/img/archer.png"
-                 alt="Archer">
+          <header :style="'background: linear-gradient(60deg,#3B3B3B 0%,' + troupe.couleur + ' 100%);'">
+            <img :src="troupe.image"
+                 :alt="troupe.nom">
           </header>
-          <div class="level" style="color: #EE5487;">
-            Niveau 3
+          <div class="level" :style="'color: ' + troupe.couleur + ';'">
+            Niveau {{troupe.niveau}}
           </div>
-          <h2 class="name">Archer</h2>
-          <button style="background-color: #EE5487;"> Former
+          <h2 class="name">{{ troupe.nom }}</h2>
+          <button :style="'background-color: ' + troupe.couleur + ';'"> Former
             <img src="/public/img/piece-or.png" alt="Former"></button>
-          <p class="description">Les archers sont des tireurs d'élite
-            qui attaquent à distance. Ils sont rapides et bon marché
-            à former, mais ils sont faibles en mêlée et doivent être
-            protégés.</p>
+          <p class="description">{{troupe.description }}</p>
           <footer>
             <div class="training"
-                 style="background-color: #EE5487;">
-              <div>25<sup>sec</sup></div>
+                 :style="'background-color: ' + troupe.couleur + ';'">
+              <div>{{troupe.formation}}<sup>sec</sup></div>
               <div>Formation</div>
             </div>
             <div class="speed"
-                 style="background-color: #EE5487;">
-              <div>24</div>
+                 :style="'background-color: ' + troupe.couleur + ';'">
+              <div>{{troupe.vitesse}}</div>
               <div>Vitesse</div>
             </div>
             <div class="cost"
-                 style="background-color: #EE5487;">
-              <div>200</div>
+                 :style="'background-color: ' + troupe.couleur + ';'">
+              <div>{{troupe.cout}}</div>
               <div>Coût</div>
             </div>
           </footer>
